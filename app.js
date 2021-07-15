cf9e9f93b07ab456eeb49643f48aa434e87ecb12
Noman Atique <natique@conestogac.on.ca>
2021-07-14 22:29:13 -0400

    Hello first

---
+++ app.js
@@ -0,0 +1,47 @@
+//OPERATORS
+// + , - , *, /, % ,
+// = Assignment Operator
+// Increment/Decrement
+
+// var x = 10;
+// console.log((x = x + 5)); //15
+// console.log(x); //  15
+
+//x += 1; //x=x+1;
+//++x; //increment
+
+//var x = 10;
+
+// console.log(--x); //9 int NO
+// console.log(x++); //9 int 10
+// console.log(++x); //11 int NO
+// console.log(x); //11
+// console.log(--x); //10
+// console.log(x++); //10 int 11
+// console.log(--x); //10 int NO
+// console.log(x++); //10 int 11
+// console.log(x); // 11
+
+var names = ["Stephen", "Chris", "Jennifer"];
+
+// console.log(names[1]);
+
+var counter = 0;
+console.log(names[counter++]);
+console.log(names[counter++]);
+console.log(names[counter]);
+
+var iteration = 0;
+console.log(iteration++);
+
+
+var firstName = "Stephen";
+var age = 23;
+var i = 10;
+
+for (var stephen = 0; stephen < 10; stephen++) {
+  console.log(stephen);
+  console.log(firstName);
+  console.log(age);
+  console.log(i);
+}
